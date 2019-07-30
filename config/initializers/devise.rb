24946b8b9b6d69ec25f105b239353dcd08747821
ducpnp <ducpnp@runsystem.net>
2019-07-30 16:34:15 +0700

    delete user birthday

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,5 +297,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
- config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], { scope: "email, user_birthday" }
+ config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], { scope: "email" }
 end
